9811b30c67584c2a88ef116be23371e960fcba81
Carryan <chenlvan@foxmail.com>
2018-01-08 10:24:33 +0800

    datetime

--- static/js/visitor-common.js
+++ static/js/visitor-common.js
@@ -26,20 +26,36 @@
 
 })($('.date-picker'));
 
-// 时间选择
-// var date_now = new Date();
-// (function(obj){
-//     obj.datetimepicker({
-//         language: 'zh-CN',
-//         autoclose: true,
-//         format: 'yyyy-mm-dd hh:ii',
-//         // endDate: date_now,
-//         initialDate: date_now,
-//         todayHighlight: true
-//     }).next().on("click", function(){
-//         $(this).prev().focus();
-//     });
-// })($('.datetime-picker'));
+//时间选择
+(function(obj){
+    if(!obj.length) { return; }
+
+    $.fn.datetimepicker.dates['zh-CN'] = {
+        days: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"],
+        daysShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+        daysMin:  ["日", "一", "二", "三", "四", "五", "六", "日"],
+        months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+        monthsShort: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一", "十二"],
+        today: "今天",
+        suffix: [],
+        meridiem: ["上午", "下午"]
+    };
+
+    var now = new Date();
+    obj.datetimepicker({
+        language: 'zh-CN',
+        autoclose: true,
+        format: 'yyyy-mm-dd hh:ii',
+        // endDate: now,
+        startView: 0,
+        // minuteStep: 1,
+        initialDate: obj.val() || now,
+        todayHighlight: true
+    }).next().on("click", function(){
+        $(this).prev().focus();
+    });
+
+})($('.datetime-picker'));
 
 
 // 下拉框
