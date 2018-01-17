001650fb37d07534622e08e732722f8fbad2bdd2
Carryan <chenlvan@foxmail.com>
2018-01-17 18:13:49 +0800

    course set

---
+++ static/js/course-common.js
@@ -0,0 +1,74 @@
+// 选择表 插件
+$.fn.selectTable = function(ops){
+    var defaults = {
+        cell: "td", //有效元素
+        disabled: ".disabled", //无法选中元素
+        selectedClass: "selected", //被选中元素的类名
+    }
+
+    var settings = $.extend({}, defaults, ops);
+    var _this = this;
+
+    _this._status = { disable: false }
+
+    function init() {
+        _this.find(settings.cell).click(function(){
+
+            if(_this._status.disable) return ;
+
+            var this_cell = $(this),
+                selectedClass = settings.selectedClass;
+
+            if(this_cell.is(settings.disabled)) return ;
+
+            if(this_cell.hasClass(selectedClass)){
+                this_cell.removeClass(selectedClass);
+            }else{
+                this_cell.addClass(selectedClass);
+            }
+        });
+    }
+
+    this.getSelected = function() { 
+        return _this.find('.'+settings.selectedClass);
+    }
+    this.getValues = function() {
+        var vals = [];
+        this.getSelected().each(function(){
+            vals.push($(this).data('val'));
+        });
+        return vals;
+    }
+    this.clearAll = function() { //清空
+        _this.find('.'+settings.selectedClass).each(function(){
+            $(this).removeClass(settings.selectedClass);
+        });
+    }
+    this.selectAll = function() { //全选
+        var enable_cells = _this.find(settings.cell).not(settings.disabled);
+        enable_cells.each(function(){
+            $(this).addClass(settings.selectedClass);
+        });
+    }
+    this.disable = function() { //禁止
+        this._status.disable = true;
+    }
+    this.enable = function() {
+        this._status.disable = false;
+    }
+
+    init();
+    return this;
+}
+
+
+// 打开modal
+$("[data-modal]").click(function(){
+    var _this = $(this),
+        title = _this.data('title'),
+        modal = _this.data('modal'),
+        $modal = $(modal);
+
+    $modal.find('.modal-title').text(title);   
+    $modal.modal('show');
+});
