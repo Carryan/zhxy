91344f34b572314abae5333447f45f442f0128be
Carryan <chenlvan@foxmail.com>
2018-01-31 18:00:59 +0800

    course all

--- static/js/course-common.js
+++ static/js/course-common.js
@@ -181,10 +181,9 @@
                 else
                 {   
                     alert_text.text(cur.attr('title'));
-                    alert.stop().fadeIn();
-                    setTimeout(function(){
-                        alert.stop().fadeOut();
-                    },3000);
+                    alert.stop().fadeIn(function(){
+                        $(this).delay(2000).fadeOut();
+                    });
                     return false;
                 }
             }
@@ -476,9 +475,8 @@ function selectModel() {
 (function(obj){
     if(!obj.length) return ;
 
-    var table = obj.data('table');
-
     obj.change(function(){
+        var table = $(this).data('table');
         if($(this).is(":checked")){
             $(table).addClass('show-msg');
         }else{
@@ -486,7 +484,7 @@ function selectModel() {
             $(".fix-table-box").scrollLeft(0);
         }
     });
-})($("#showTeacher"));
+})($("#showTeacher, .show-teacher"));
 
 // 显示年级
 (function(obj){
@@ -511,7 +509,7 @@ function selectModel() {
     if(!obj.length) return ;
 
     obj.find('select').change(function(){
-        var cm = ['一','二','三','四','五','六','七','八','九','十','十一','十二','十三','十四'],
+        var cm = ['一','二','三','四','五','六','七','八','九','十','十一','十二','十三','十四','十五','十六','十七','十八'],
             i = 0;
         obj.find('select').each(function(){
             var list = '', j = 0;
