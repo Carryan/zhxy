3d7222b52973f41b5a7da0342afcee64a8a648c0
Carryan <chenlvan@foxmail.com>
2018-02-05 17:46:13 +0800

    course

--- static/js/course-common.js
+++ static/js/course-common.js
@@ -1,4 +1,24 @@
 
+// indexOf 兼容性
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (elt /*, from*/) {
+        var len = this.length >>> 0;
+        var from = Number(arguments[1]) || 0;
+        from = (from < 0)
+            ? Math.ceil(from)
+            : Math.floor(from);
+        if (from < 0)
+            from += len;
+        for (; from < len; from++) {
+            if (from in this &&
+                this[from] === elt)
+                return from;
+        }
+        return -1;
+    };
+}
+
+
 // 可拖拽表
 ; (function (obj) {
     if (!obj.length) return false;
