db3e64cc3affbc61910bfd5f029b0671d7dc3ba4
Carryan <chenlvan@foxmail.com>
2018-04-28 17:38:10 +0800

    support

--- support/static/js/common-support.js
+++ support/static/js/common-support.js
@@ -1,12 +1,76 @@
 
+var treeItem = {
+    name: 'tree-item',
+    template: '#tree-item-template',
+    props: {
+        model: Object, //规定model的类型
+        index: [String, Number],
+        parent: Object,
+        moveurl: String,
+        treedata: Object
+    },
+    data: function() {
+        return {
+            open: false
+        }
+    },
+    computed: {
+        isFolder: function () {
+            return this.model.children &&
+                this.model.children.length
+        }
+    },
+    methods: {
+        toggle: function () {
+            if (this.isFolder) {
+                this.open = !this.open
+            }
+        },
+        orderChange: function (arr, index, tindex) {
+            if (tindex < 0 || tindex >= arr.length) return;
+            var url = this.moveurl;
+            var data = {
+                "from": index,
+                "to": tindex
+            }
+            $.ajax({
+                url: url,
+                type: "GET",
+                dataType: "json",
+                data: data,
+                success: function (res) {
+                    if (res.state === "ok") {
+                        arrOrderMove(arr, index, tindex);
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.log(error);
+                }
+            });
+        },
+        deleteItem: function (parent, index) {
+            this.$emit("delete", parent, index);
+        },
+        addItem: function (nodeId) {
+            this.$emit("add", nodeId);
+        },
+        editItem: function () {
+            this.$emit("edit");
+        }
+
+    }
+}
+
+
 var spItem = new Vue({
     el: "#spContent",
     data: {
         "items": [],
-        "details": []
+        "details": {},
+        "detailContent": {}
     },
     components: {
-       
+        "tree-item": treeItem
     },
     computed:{
         
@@ -15,6 +79,11 @@ var spItem = new Vue({
         // 激活选项
         optionActive: function(id, item) {
             item.activeId = id;
+            var active = [];
+            this.items.forEach(function(v){
+                active.push(v.activeId);
+            });
+            this.$emit("optionActive", active);
         },
         // 删除选项
         optionRemove: function (index, editselected) {
@@ -77,6 +146,64 @@ var spItem = new Vue({
                 }
             });
             return ops;
+        },
+        // 删除
+        delContent: function (parent, index) {
+            this.$emit("delContent", parent, index);
+        },
+        // 添加
+        addContent: function (nodeId) {
+            // this.$emit("addContent");
+            var node = getNode(this.details.treeData, nodeId);
+            console.log(node);
+            var select = [];
+            if(node.parentNode){
+                node.parentNode.children.forEach(function(v){
+                    select.push({
+                        "name": v.name,
+                        "val": v.id
+                    });
+                });
+                this.detailContent = {
+                    "isShow": true,
+                    "title": "添加",
+                    "input": [
+                        {
+                            "name": "目录名称",
+                            "value": ""
+                        },
+                        {
+                            "name": "上级目录",
+                            "value": node.node.id,
+                            "select": select
+                        }
+                    ]
+                }
+            }else{
+                this.detailContent = {
+                    "isShow": true,
+                    "title": "添加",
+                    "input": [
+                        {
+                            "name": "目录名称",
+                            "value": ""
+                        }
+                    ]
+                }
+            }
+            
+        },
+        // 另存为
+        saveAs: function() {
+            this.$emit("saveAs");
+        },
+        // 编辑
+        editContent: function() {
+            this.$emit("editContent");
+        },
+        // 提交
+        submitContent: function() {
+            this.$emit("submitContent");
         }
         
     }
@@ -95,4 +222,59 @@ function getIdsArr(arr, except) {
             return v.id;
         });
     }
+}
+
+// 数组序号移动
+function arrOrderMove(arr, index, tindex) {
+    if (index > tindex) {
+        arr.splice(tindex, 0, arr[index]);
+        arr.splice(index + 1, 1)
+    } else {
+        arr.splice(tindex + 1, 0, arr[index]);
+        arr.splice(index, 1)
+    }
+}
+
+
+// 根据NodeID查找当前节点以及父节点
+function getNode(json, nodeId) {
+    var parentNode = null;
+    var node = null;
+
+    function nodes(json, nodeId){
+        //1.第一层 root 深度遍历整个JSON
+        for (var i = 0; i < json.length; i++) {
+            if (node) {
+                break;
+            }
+            var obj = json[i];
+            //没有就下一个
+            if (!obj || !obj.id) {
+                continue;
+            }
+            //2.有节点就开始找，一直递归下去
+            if (obj.id == nodeId) {
+                node = obj; //找到了与nodeId匹配的节点，结束递归
+                break;
+            } else {
+                //3.如果有子节点就开始找
+                if (obj.children) {
+                    parentNode = obj; //4.递归前，记录当前节点，作为parent 父亲
+                    nodes(obj.children, nodeId);  //递归往下找
+                } else {
+                    continue; //跳出当前递归，返回上层递归
+                }
+            }
+        }
+    }
+    nodes(json, nodeId);
+    
+    if (!node) {  //5.如果木有找到父节点，置为null，因为没有父亲  
+        parentNode = null;
+    }
+
+    return {
+        parentNode: parentNode,
+        node: node
+    };
 }
